d0bd30d6f467cf6e36b275671ba694ae942a7cd6
Dev P. Sharma <44609609+codewithdev@users.noreply.github.com>
2020-05-05 14:22:40 +0530

    Add files via upload

---
+++ .commans/register.php
@@ -0,0 +1,8 @@
+<?php include('function.php')?>
+
+<form method="post" action= "register.php">
+	<?php echo display_error(); ?>
+
+<input type="email" name="email" value="<?php echo $email; ?>">
+<input type="password" name="password" value="<?php echo $password; ?>">	
+<input type="password" name="password" value="<?php echo $password; ?>">	
